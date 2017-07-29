1ce81ba3e0cf5f5bf6975b17e507c58f8df97220
JrodManU <jrodmanu@yahoo.com>
2017-07-28 22:38:40 -0400

    worked on maze

--- js/genButtonClick.js
+++ js/genButtonClick.js
@@ -1,5 +1,13 @@
 $(document).ready(function() {
   $("#genButton").click(function() {
-    generateMaze();
+    var maze = generateMaze();
+    var ctx = $("#maze")[0].getContext("2d");
+    var size = maze.length;
+    for(var x = 0; x < size; x++) {
+      for(var y = 0; y < size; y++) {
+        ctx.fillRect(x*10,y*10,10,10);
+      }
+    }
+
   });
 });
--- js/mazeGen.js
+++ js/mazeGen.js
@@ -2,13 +2,20 @@
 //false means there is a wall
 //or that the area has not been reviewed
 var mazeSize = 50;
-var mazeSizeWithWalls = mazeSize * 2 + 1;
+var mazeSizeWithWalls = withWallSize(mazeSize);
 var cells;
+var cellStack;
 var maze;
+var startX = 0;
+var startY = 0;
+var endPosX = mazeSize - 1;
+var endPosY = mazeSize - 1;
 
 function generateMaze() {
   cells = booleanPlane(mazeSize);
+  cellStack = [];
   maze = booleanPlane(mazeSizeWithWalls);
+  flood(startX, startY);
   return maze;
 }
 
@@ -24,7 +31,35 @@ function booleanPlane(size) {
 
   return plane;
 }
+function getNeighbors(x, y) {
+  var neighbors = [] ;
 
+  neighbors.push([x+1,y]) if(isValid(x+1,y));
+  neighbors.push([x,y-1]) if(isValid(x,y-1));
+  neighbors.push([x,y+1]) if(isValid(x,y+1));
+  neighbors.push([x-1,y]) if(isValid(x-1,y));
+
+  return neighbors;
+}
+function isValid(x,y){
+  return x > -1 && y > -1 && x < mazeSize && y < mazeSize
+}
 function flood(x, y) {
+  
 
+  var neighbors = getNeighbors(x,y);
+  var chosenOne;
+  if(neighbors.length == 0) {
+    cellStack.pop();
+    if(cellStack.length == 0) {
+      return;
+    }
+    chosenOne = cellStack[cellStack.length - 1];
+  }else {
+    chosenOne = neighbors[Math.floor(Math.random()*neighbors.length)];
+  }
+  flood(chosenOne[0], chosenOne[1]);
+}
+function withWallSize(number) {
+  return number * 2 + 1;
 }
